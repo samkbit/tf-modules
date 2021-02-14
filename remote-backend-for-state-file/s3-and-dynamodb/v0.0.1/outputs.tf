ba7fa680a28720dac7b955d055782352a8d4b162
Sam <50801934+samkbit@users.noreply.github.com>
2021-02-14 14:17:09 -0500

    Delete outputs.tf

--- remote-backend-for-state-file/s3-and-dynamodb/v0.0.1/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "s3_bucket_arn" {
-  value       = aws_s3_bucket.terraform_state.arn
-  description = "AWS S3 bucket ARN for Terraform state"
-}
-
-output "dynamodb_table_name" {
-  value       = aws_dynamodb_table.terraform_locks.name
-  description = "AWS DynamoDB table name for Terraform locks"
-}
